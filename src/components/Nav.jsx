707aba70a22907f8c88136a8dad52549b814cec2
acsolabs <sofian.achbabi@gmail.com>
2026-04-17 17:58:17 +0200

    ✨ HOME > create Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <div className="menu">
+      <ul>
+        <li>Home</li>
+        <li>Ajouter Techno</li>
+        <li>Toutes les Technos</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
