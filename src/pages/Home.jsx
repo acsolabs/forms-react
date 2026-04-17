1f295c12832849fc9946767ab2e57d0597d359bc
acsolabs <sofian.achbabi@gmail.com>
2026-04-17 17:57:03 +0200

    ✨ HOME > create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Ma Progression</h1>
+      <div>
+        Bienvenue sur le site consacré à ta progression en programmation
+      </div>
+    </div>
+  );
+};
+
+export default Home;
