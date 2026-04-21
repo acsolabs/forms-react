91cad69c31bb68576dae544ff085235d88bf7910
acsolabs <sofian.achbabi@gmail.com>
2026-04-21 23:20:11 +0200

    💄 NewTechno : Add page Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import NewTechno from "./pages/NewTechno";
 import "./styles/app.css";
 const App = () => {
   return (
     <>
       <Nav />
-      <Home />;
+      <Home />
+      <NewTechno />
     </>
   );
 };
