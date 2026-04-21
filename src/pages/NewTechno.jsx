25badc4131871de32cba057770f21bfbca3b687d
acsolabs <sofian.achbabi@gmail.com>
2026-04-21 23:19:21 +0200

    ✨ NewTechno > add component NewTechno with form

--- src/pages/NewTechno.jsx
+++ src/pages/NewTechno.jsx
@@ -1,5 +1,20 @@
+import Nav from "../components/Nav";
+
 const NewTechno = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="techno-add">
+        <h1>Ajouter une Nouvelle Techno </h1>
+        <form action="">
+          <label htmlFor="techno-name">Nom :</label>
+          <br />
+          <input type="text" name="techno-name" />
+          <br />
+          <input type="submit" value="Ajouter" />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default NewTechno;
