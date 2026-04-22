3c327feb7b4aa007a55a1942983e36ea45265a4f
acsolabs <sofian.achbabi@gmail.com>
2026-04-22 14:35:03 +0200

    ✨ TechnoList : add TechnoList Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import NewTechno from "./pages/NewTechno";
+import TechnoList from "./pages/TechnoList";
 import "./styles/app.css";
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Nav />
       <Home />
       <NewTechno />
+      <TechnoList />
     </>
   );
 };
---
+++ src/pages/TechnoList.jsx
@@ -0,0 +1,10 @@
+const TechnoList = () => {
+  return (
+    <div className="techno-list">
+      <h1>Toutes les Technos</h1>
+      <p>Afficher toutes les technos </p>
+    </div>
+  );
+};
+
+export default TechnoList;
