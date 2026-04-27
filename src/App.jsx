3d904a899af3f3f908c41baed535d96a80f2a668
acsolabs <sofian.achbabi@gmail.com>
2026-04-27 21:19:30 +0200

    ✨ App : add Routes, Route  & handleNewTechno

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import NewTechno from "./pages/NewTechno";
 import TechnoList from "./pages/TechnoList";
 import "./styles/app.css";
+import { useState } from "react";
 const App = () => {
+  const [technos, setTechnos] = useState([]);
+
+  const handleNewTechno = (techno) => {
+    console.log("handleNewTechno", techno);
+  };
+
   return (
     <>
       <Nav />
-      <Home />
-      <NewTechno />
-      <TechnoList />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/add"
+          element={<NewTechno handleNewTechno={handleNewTechno} />}
+        ></Route>
+        <Route path="/list" element={<TechnoList />}></Route>
+      </Routes>
     </>
   );
 };
